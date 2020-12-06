dcf6b02d101577f424e678433a3140e9ad07d28c
opticalpvpx <jpduddleston@cps.edu>
2020-12-05 21:46:12 -0600

    Real quick fix

--- HeliosTheme/build.sh
+++ HeliosTheme/build.sh
@@ -21,8 +21,8 @@ if [ ! -f "artisan" ]; then
     zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
 
     echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/TheFonix/Pterodactyl-Themes.git .
-    cp -r MasterThemes/LimeStitch/public ..
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/LegendNetwork/PanelTheme.git .
+    cp -r HeliosTheme/public ..
     
 
     echo "Files have been copied over!"
